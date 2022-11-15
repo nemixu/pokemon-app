843c370d2df47e51e7757198cb89e08c41c20729
nemixu <s.r.seagrave@gmail.com>
2022-11-15 11:29:32 +0000

    inital commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    
+    return(
+        <>
+            <div className="container">
+                Test
+            </div>
+        </>
+    )
+}
+
+export default Home;
