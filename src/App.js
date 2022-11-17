effe49dfbb9be0f2914325caa44605e32d0382f2
nemixu <s.r.seagrave@gmail.com>
2022-11-17 22:38:21 +0000

    Add router for pathing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from "react"
 import Home from './Components/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PokemonDetail from './Components/PokemonDetail';
 
 function App() {
   return (
-    <Home/>
+    //react router, using Routes instead of Switch as vers6 of router uses this and takes an element property.
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/pokemon-detail/:id" element={<PokemonDetail/>} />
+      </Routes>
+    </Router>
   );
 }
 
