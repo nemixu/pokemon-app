7ee6787c5179b0779ad2f54207475d6019dd0024
nemixu <s.r.seagrave@gmail.com>
2022-11-20 14:40:33 +0000

    Add offset props to link

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PokemonDetail from "./PokemonDetail";
-
-const Card = ({pokemon, loading}) => {
+//re name card to pokelist or something descriptive  pokeball spinner#
+//prettier and eslint & configure
+// do unit tests jest react testing library
+const Card = ({pokemon, loading, currentOffset}) => {
     return (
         <>
         {
             loading ? <h1>Please wait loading...</h1> : 
                 pokemon.map((item)=>{
-                    return(                    
-                        <div className="card" key={item.id}>
-                        <Link to={`/pokemon-detail/${item.id}`} state={{ item , pokemon}}>
-                            <div className="card-inner-container">
-                                <h2>{item.id}</h2>
+                    return(                 
+                        <div className="col-sm-12 col-md-6 col-lg-3" key={item.id}>
+                        <div className="pokemon-card">
+                        <Link to={`/pokemon-detail/${item.id}`} state={{ item , pokemon, currentOffset}}>
+                            <div className="card-inner-container text-center text-color">
+                                <h2 className="fs-3"><span className="fs-6">CP </span>{item.base_experience}</h2>
                                 <img src={item.sprites.front_default} alt="of a pokemon"/>
                                 <h3>{item.name}</h3>
                             </div>
                             </Link>
                         </div>
+                        </div>   
+                        
                 )})       
         }
         </>
