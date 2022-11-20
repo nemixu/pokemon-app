001ee5b8b66f3135d50716d8d0abf7542a45e47c
nemixu <s.r.seagrave@gmail.com>
2022-11-20 14:41:52 +0000

    Add a button click handler to navigate back with path and search params.

--- src/Components/PokemonDetail.jsx
+++ src/Components/PokemonDetail.jsx
@@ -6,37 +6,93 @@ import Home from "./Home";
 const PokemonDetail = () => {
     const {state} = useLocation();
     const navigate = useNavigate();
-    console.log(state)
+    const type = state.item.types[0].type.name;
+    const handleBackClick = () => {
+            navigate ({
+                pathname: '/',
+                search: `?offset=${state.currentOffset}`,
+            })
+    }
 
     return (
         <>
             {
-                (!state.item) ? <div key={Math.random}>Oops im empty</div> : (
-                    <>
-                    <div className="pokemon-detail-wrapper" key={state.item.name}>
-                        <h1>
-                            {state.item.name}
-                        </h1>
-                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${state.item.id}.svg`} alt="pokemon"/>
-                        <div className="abilities">
-                            {
-                                state.item.abilities.map(x => {
-                                    return(
-                                        <>
-                                            <div>
-                                                <h2>{x.ability.name}</h2>
+                (!state.item) ? <div>Oops im empty</div> : (
+
+                        <div className="container-fluid" key={state.item.id}>
+                            <div className="row">
+                                <div className="col-12">
+                                    <button className="btn btn-danger" onClick={handleBackClick}>Go Back</button>
+                                </div>
+                                <div className="col-12">
+                                    <div className="pokemon-detail-wrapper">
+                                        <div className={`pokemon-detail-wrapper-card type-${type}`}>
+                                            <div className="img-container">
+                                                <img className="pokemon-detail-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${state.item.id}.svg`} alt="pokemon"/>
                                             </div>
-                                        </>
-                                    )
-                                })
-                            }
+                                        
+                                            <div className="pokemon-details">
+                                                <h1 className="text-capitalize">
+                                                    <span>{state.item.name}</span>
+                                                </h1>
+                                                <h2 className="text-center">
+                                                    <span className="d-block">#{state.item.id}</span>
+                                                    <span>Weight{state.item.weight}</span>
+                                                </h2>
+                                            </div>
+                                            <div className="types">{
+                                                state.item.types.map(typeArray => {
+                                                    return(
+                                                        <div className="d-inline px-2">
+                                                            {typeArray.type.name}
+                                                        </div>
+                                                )})
+                                            }
+                                               
+                                            </div>
+                                            
+                                            <div className="abilities">
+                                                {
+                                                    state.item.abilities.map(x => {
+                                                        return(
+                                                            <>
+                                                                <div className="d-inline-block px-3">
+                                                                    <h2 className="text-capitalize fs-4">{x.ability.name}</h2>
+                                                                </div>
+                                                            </>
+                                                    )})
+                                                }
+                                            </div>
+                                            <div className="stats justify-content-center align-items-center mb-4">
+                                                {
+                                                    state.item.stats.map(statsArray =>{
+                                                        return(
+                                                            <div className="col-12">
+                                                                <div className="d-block">
+                                                                    <div className="px-2 d-inline">
+                                                                        
+                                                                        {statsArray.stat.name}
+                                                                    
+                                                                    </div>
+                                                                    <div className="stat-background">
+                                                                        <div className={`px-2 d-inline stat-name-fillbar type-${type} `} style={{width:statsArray.base_stat + '%',}}>
+                                                                        {statsArray.base_stat} 
+                                                                        </div>
+                                                                    </div>
+                                                                    
+                                                                </div>
+                                                            </div>
+                                                            
+                                                                
+                                                        )
+                                                    })
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <button onClick={() => navigate("/")}>Go Back</button>
-                    
-                    </>
-                    
-                    
                 )
             }
         </>
