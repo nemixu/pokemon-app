d986f1e8dc68655d08042f5c42c75128311ffe3f
nemixu <s.r.seagrave@gmail.com>
2022-11-20 14:41:17 +0000

    Rework solution, to use search params to set the offset, so we can have the offset and go back to the page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,22 +1,24 @@
 import React from "react"
 import axios from "axios";
 import { useState, useEffect  } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 import Card from "./Card";
-import Pokemon from "./Pokemon";
 
 const Home = () => {
 
     // On mount make initial req to pokemon api of max 20
+    const [searchParams, setSearchParams] = useSearchParams();
     const [pokemonData, setPokemonData] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [endpoint, setEndpoint] = useState("https://pokeapi.co/api/v2/pokemon/");
-    const [page, setPage] = useState(true);
-
+    const [offset, setOffset] = useState(searchParams.get('offset') || 0);
+    const pokeApi = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}`
+    
+    
     const requestPokemon = async () => {
         setLoading(true);
-        const data  = await axios.get(endpoint)
+        const data  = await axios.get(pokeApi)
         getAllPokemon(data.data.results);
-        setPage({nextPage: data.data.next, previousPage: data.data.previous})
+        setSearchParams({offset})
         setLoading(false);
     }
     
@@ -26,42 +28,44 @@ const Home = () => {
             const result = await axios.get(item.url)
             setPokemonData(receivedPokemon =>{
                 receivedPokemon = [...receivedPokemon, result.data]
-
                 receivedPokemon.sort((a, b)=> a.id > b.id? 1: - 1)
                 return receivedPokemon;
             })
         })
-    }
-    
+    }    
     
     // lifecycle methods -> allows to do things as part of the rendering cycle of the dom.
     // any time endpoint changes, recall
     useEffect(()=> {
         requestPokemon();
-    },[endpoint]);
+    },[offset]);
 
     return(
         <>
-            <div className="card-container">
-                <Card pokemon={pokemonData} loading={loading} />
-                <Pokemon/>
-            </div>
-            <div>
+            <div className="pt-3 text-center container">
                 <div className="buttons">
                 {
-                    !page.previousPage ? <button className="btn btn-success" disabled>Previous</button> :<button className="btn btn-success" onClick={() => {
+                    offset == 0 && "0" ? 
+                    <button className="btn btn-success" disabled>Previous</button> : 
+                    <button className="btn btn-success" onClick={() => {
                             setPokemonData([])
-                            setEndpoint(page.previousPage)
-                        }}
-                        >Previous</button>
+                            setOffset(offset -20)
+                        }}>Previous</button>
                 }
+                </div>
+                <div className="buttons">
                 {
-                        page.nextPage && <button onClick={() => {
+                        offset >= 0 && <button onClick={() => {
                             setPokemonData([])
-                            setEndpoint(page.nextPage)
+                            setOffset(Number(offset) +20)
                         }} className="btn btn-success">Next</button>
                 }
                 </div>
+            </div>
+            <div className="card-container container">
+                <div className="row">
+                    <Card pokemon={pokemonData} loading={loading} currentOffset={offset}/>     
+                </div>       
             </div>
         </>
     )
