273ea4ba87bac694345e5cafec5e26c7b10e4979
nemixu <s.r.seagrave@gmail.com>
2022-11-21 16:44:50 +0000

    Formatting, add font family, basic styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
-import React from "react"
-import Home from './Components/Home'
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
 import PokemonDetail from './Components/PokemonDetail';
 
 function App() {
-  return (
-    //react router, using Routes instead of Switch as vers6 of router uses this and takes an element property.
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/:offset?" element={<Home/>} />
-        <Route path="/pokemon-detail/:id" element={<PokemonDetail/>} />
-      </Routes>
-    </Router>
-  );
+	return (
+		// react router, using Routes instead of Switch as vers6 of router uses this and takes an element property.
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/:offset?" element={<Home />} />
+				<Route path="/pokemon-detail/:id" element={<PokemonDetail />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
