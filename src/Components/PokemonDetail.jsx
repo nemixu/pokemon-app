4e4afcb104728665305de1dcccbe33da67fe567c
nemixu <s.r.seagrave@gmail.com>
2022-11-23 08:57:25 +0000

    Add styling and keys .

--- src/Components/PokemonDetail.jsx
+++ src/Components/PokemonDetail.jsx
@@ -25,7 +25,7 @@ function PokemonDetail() {
 								label="Go back"
 								handleClick={handleBackClick}
 								isDisabled={false}
-								className="btn btn-danger"
+								className="btn btn-success mt-3"
 							/>
 						</div>
 						<div className="col-12">
@@ -41,12 +41,16 @@ function PokemonDetail() {
 									<div className="detail-container">
 										<div className="pokemon-details">
 											<h1 className="text-capitalize text-center">
-												<span className="fs-2">{state.item.name}</span>
+												<span className="fs-2 text-capitalize fw-bold ">
+													{state.item.name}
+												</span>
 											</h1>
 										</div>
-										<div className="types text-center">
+										<div className="types text-center pb-2">
 											{state.item.types.map((typeArray) => (
-												<div className="d-inline px-2 fs-5">
+												<div
+													className="d-inline px-2 fs-5 text-capitalize fst-italic"
+													key={typeArray.id}>
 													{typeArray.type.name}
 												</div>
 											))}
@@ -54,7 +58,7 @@ function PokemonDetail() {
 
 										<div className="abilities">
 											{state.item.abilities.map((x) => (
-												<div className="d-inline-block px-3">
+												<div className="d-inline-block px-3" key={x.id}>
 													<h2 className="text-capitalize fs-5">
 														{x.ability.name}
 													</h2>
@@ -63,7 +67,7 @@ function PokemonDetail() {
 										</div>
 										<div className="stats justify-content-center align-items-center mb-4">
 											{state.item.stats.map((statsArray) => (
-												<div className="col-12">
+												<div className="col-12" key={statsArray.id}>
 													<div className="d-block">
 														<div className="px-2 d-inline text-capitalize">
 															{statsArray.stat.name}
